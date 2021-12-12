18243405b2ae698064c050e561735d9860730756
Carol Leung <carol.carl.leung@gmail.com>
2021-12-12 17:05:14 +0100

    fix:  use regular node.js module for next config

--- next.config.js
+++ next.config.js
@@ -1,2 +1,8 @@
-/** @type {import('next').NextConfig} */
-export const reactStrictMode = true;
+/**
+ * @type {import('next').NextConfig}
+ */
+ const nextConfig = {
+    /* config options here */
+  }
+  
+  module.exports = nextConfig;
